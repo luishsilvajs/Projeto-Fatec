da28c92c3c9f0f26d7763996a3d546608c8feaba
luishsilvajs <luishs.dev@gmail.com>
2023-05-16 21:35:04 -0300

    insert event collection

--- JavaScript/firebase/dados.js
+++ JavaScript/firebase/dados.js
@@ -1,6 +1,22 @@
 function salvar (event, collection){
     event.preventDefault()
     if(document.getElementById('nome').value === ''){
-        alert('Campo nome deve ser preenchido.')
+        alerta('Campo Nome deve ser preenchido.')
+    }
+    else if (document.getElementById('email').value === ''){
+        alerta('Campo Email deve ser preenchido.')
+    }
+    else if (document.getElementById('cpf').value === ''){
+        alerta('Campo CPF deve ser preenchido.')
+    }
+    else if (document.getElementById('peso').value < 0){
+        alerta('Campo Peso deve ser preenchido.')
+    }
+    else if (document.getElementById('altura').value < 0 ||
+             document.getElementById('altura').value > 3){
+        alerta('Campo Email deve ser preenchido.')
+    }
+    else {
+        incluir(event, collection)
     }
 }
